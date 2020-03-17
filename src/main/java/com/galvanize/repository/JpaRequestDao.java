be41c8954adab30f3e09ba445b23776c4ec689e0
AlexanderLundin <54223212+AlexanderLundin@users.noreply.github.com>
2020-03-17 14:57:42 -0700

    removed javadoc notation from JpaRequestDao

--- src/main/java/com/galvanize/repository/JpaRequestDao.java
+++ src/main/java/com/galvanize/repository/JpaRequestDao.java
@@ -23,11 +23,10 @@ public class JpaRequestDao implements RequestDao {
 
     private final static String REQUEST_FIND_ALL = "select r from Request r";
 
-    /**
-     * Returns the number of entities available.
-     *
-     * @return the number of entities.
-     */
+
+    // READ
+
+
     @Override
     public long count() {
         Query query = em.createQuery(REQUEST_FIND_ALL);
@@ -49,12 +48,6 @@ public class JpaRequestDao implements RequestDao {
         return null;
     }
 
-    /**
-     * Returns a {@link Page} of entities meeting the paging restriction provided in the {@code Pageable} object.
-     *
-     * @param pageable
-     * @return a page of entities
-     */
     @Override
     public Page<Request> findAll(Pageable pageable) {
         return null;
@@ -65,56 +58,26 @@ public class JpaRequestDao implements RequestDao {
         return null;
     }
 
-
-    /**
-     * Deletes the entity with the given id.
-     *
-     * @param integer must not be {@literal null}.
-     * @throws IllegalArgumentException in case the given {@literal id} is {@literal null}
-     */
     @Override
     public void deleteById(Integer integer) {
 
     }
 
-    /**
-     * Deletes a given entity.
-     *
-     * @param entity must not be {@literal null}.
-     * @throws IllegalArgumentException in case the given entity is {@literal null}.
-     */
     @Override
     public void delete(Request entity) {
 
     }
 
-    /**
-     * Deletes the given entities.
-     *
-     * @param entities must not be {@literal null}. Must not contain {@literal null} elements.
-     * @throws IllegalArgumentException in case the given {@literal entities} or one of its entities is {@literal null}.
-     */
     @Override
     public void deleteAll(Iterable<? extends Request> entities) {
 
     }
 
-    /**
-     * Deletes all entities managed by the repository.
-     */
     @Override
     public void deleteAll() {
 
     }
 
-    /**
-     * Saves a given entity. Use the returned instance for further operations as the save operation might have changed the
-     * entity instance completely.
-     *
-     * @param entity must not be {@literal null}.
-     * @return the saved entity; will never be {@literal null}.
-     * @throws IllegalArgumentException in case the given {@literal entity} is {@literal null}.
-     */
     @Override
     public <S extends Request> S save(S entity) {
         return null;
@@ -125,90 +88,41 @@ public class JpaRequestDao implements RequestDao {
         return null;
     }
 
-    /**
-     * Retrieves an entity by its id.
-     *
-     * @param integer must not be {@literal null}.
-     * @return the entity with the given id or {@literal Optional#empty()} if none found.
-     * @throws IllegalArgumentException if {@literal id} is {@literal null}.
-     */
     @Override
     public Optional<Request> findById(Integer integer) {
         return Optional.empty();
     }
 
-    /**
-     * Returns whether an entity with the given id exists.
-     *
-     * @param integer must not be {@literal null}.
-     * @return {@literal true} if an entity with the given id exists, {@literal false} otherwise.
-     * @throws IllegalArgumentException if {@literal id} is {@literal null}.
-     */
     @Override
     public boolean existsById(Integer integer) {
         return false;
     }
 
-    /**
-     * Flushes all pending changes to the database.
-     */
     @Override
     public void flush() {
 
     }
 
-    /**
-     * Saves an entity and flushes changes instantly.
-     *
-     * @param entity
-     * @return the saved entity
-     */
     @Override
     public <S extends Request> S saveAndFlush(S entity) {
         return null;
     }
 
-    /**
-     * Deletes the given entities in a batch which means it will create a single {@link Query}. Assume that we will clear
-     * the {@link EntityManager} after the call.
-     *
-     * @param entities
-     */
     @Override
     public void deleteInBatch(Iterable<Request> entities) {
 
     }
 
-    /**
-     * Deletes all entities in a batch call.
-     */
     @Override
     public void deleteAllInBatch() {
 
     }
 
-    /**
-     * Returns a reference to the entity with the given identifier. Depending on how the JPA persistence provider is
-     * implemented this is very likely to always return an instance and throw an
-     * {@link EntityNotFoundException} on first access. Some of them will reject invalid identifiers
-     * immediately.
-     *
-     * @param integer must not be {@literal null}.
-     * @return a reference to the entity with the given identifier.
-     * @see EntityManager#getReference(Class, Object) for details on when an exception is thrown.
-     */
     @Override
     public Request getOne(Integer integer) {
         return null;
     }
 
-    /**
-     * Returns a single entity matching the given {@link Example} or {@literal null} if none was found.
-     *
-     * @param example must not be {@literal null}.
-     * @return a single entity matching the given {@link Example} or {@link Optional#empty()} if none was found.
-     * @throws IncorrectResultSizeDataAccessException if the Example yields more than one result.
-     */
     @Override
     public <S extends Request> Optional<S> findOne(Example<S> example) {
         return Optional.empty();
@@ -224,36 +138,16 @@ public class JpaRequestDao implements RequestDao {
         return null;
     }
 
-    /**
-     * Returns a {@link Page} of entities matching the given {@link Example}. In case no match could be found, an empty
-     * {@link Page} is returned.
-     *
-     * @param example  must not be {@literal null}.
-     * @param pageable can be {@literal null}.
-     * @return a {@link Page} of entities matching the given {@link Example}.
-     */
     @Override
     public <S extends Request> Page<S> findAll(Example<S> example, Pageable pageable) {
         return null;
     }
 
-    /**
-     * Returns the number of instances matching the given {@link Example}.
-     *
-     * @param example the {@link Example} to count instances for. Must not be {@literal null}.
-     * @return the number of instances matching the {@link Example}.
-     */
     @Override
     public <S extends Request> long count(Example<S> example) {
         return 0;
     }
-
-    /**
-     * Checks whether the data store contains elements that match the given {@link Example}.
-     *
-     * @param example the {@link Example} to use for the existence check. Must not be {@literal null}.
-     * @return {@literal true} if the data store contains elements that match the given {@link Example}.
-     */
+    
     @Override
     public <S extends Request> boolean exists(Example<S> example) {
         return false;
