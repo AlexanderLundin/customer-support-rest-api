e7cb09b1dac9f76c006fa307cfe863c8dcf94ce1
AlexanderLundin <54223212+AlexanderLundin@users.noreply.github.com>
2020-03-18 08:13:21 -0700

    add request_notes table that references customer_request

--- src/main/resources/mysql-data-load.sql
+++ src/main/resources/mysql-data-load.sql
@@ -14,6 +14,15 @@ CREATE TABLE customer_requests (
     requestStatus   VARCHAR(20),
     PRIMARY KEY (requestNumber)
 );
+DROP TABLE IF EXISTS request_notes;
+CREATE TABLE request_notes (
+       noteId   INTEGER AUTO_INCREMENT,
+       dateTime VARCHAR(20),
+       notes VARCHAR(50),
+       requestNumber INTEGER,
+       FOREIGN KEY (requestNumber) REFERENCES customer_requests (requestNumber),
+       PRIMARY KEY (noteId)
+);
 
 INSERT INTO customer_requests(requestDateTime, customerName, customerAddress, phoneNumber, description, technician, appointmentDate, requestStatus)
 VALUES('03/16/2020 6:07PM', 'Some Customer1', '123 Any Street, SomeCity, ST, 999991', '111-222-33331', 'its broke and I need it fixed!1', 'tech1', 'appt1', 'ASSIGNED');
@@ -27,4 +36,9 @@ INSERT INTO customer_requests(requestDateTime, customerName, customerAddress, ph
 VALUES('03/16/2020 11:00PM', 'Some Customer5', '123 Any Street, SomeCity, ST, 999991', '111-222-33331', 'its broke and I need it fixed!5', 'tech5', 'appt5', 'ASSIGNED');
 INSERT INTO customer_requests(requestDateTime, customerName, customerAddress, phoneNumber, description, technician, appointmentDate, requestStatus)
 VALUES('03/16/2020 12:00PM', 'Some Customer6', '123 Any Street, SomeCity, ST, 999992', '111-222-33332', 'its broke and I need it fixed!6', 'tech6', 'appt6', 'RESOLVED');
+INSERT INTO request_notes(dateTime, notes, requestNumber)
+VALUES('03/16/2020 6:07PM', 'Some Notes', 1);
+INSERT INTO request_notes(dateTime, notes, requestNumber)
+VALUES('03/16/2020 6:08PM', 'Some Notes2', 1);
 select * from customer_requests;
+select * from request_notes;
