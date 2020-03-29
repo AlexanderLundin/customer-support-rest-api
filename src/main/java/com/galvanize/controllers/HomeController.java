65bba583f152b1f45dffd2ac07ca6c1bd750f806
AlexanderLundin <54223212+AlexanderLundin@users.noreply.github.com>
2020-03-28 21:50:20 -0700

    refactor home controller to work with jpa only

--- src/main/java/com/galvanize/controllers/HomeController.java
+++ src/main/java/com/galvanize/controllers/HomeController.java
@@ -1,37 +1,35 @@
-//package com.galvanize.controllers;
-//
-//import com.galvanize.repository.JpaRequestDao;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.jdbc.core.JdbcTemplate;
-//import org.springframework.ui.Model;
-//import org.springframework.ui.ModelMap;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//import org.springframework.web.servlet.ModelAndView;
-//
-//@RestController
-//public class HomeController {
-//
-//    private JdbcRequestDao jdbcRequestDao;
-//    private JpaRequestDao jpaRequestDao;
-//
-//    @Value("${spring.application.name}")
-//    String appName;
-//
-//    public HomeController(JdbcTemplate jdbcTemplate, JpaRequestDao jpaRequestDao){
-//        this.jdbcRequestDao = new JdbcRequestDao(jdbcTemplate);
-//        this.jpaRequestDao = jpaRequestDao;
-//    }
-//
-//    @RequestMapping("/")
-//    public ModelAndView redirectWithUsingRedirectPrefix(ModelMap model) {
-//        return new ModelAndView("redirect:/home", model);
-//    }
-//
-//    @RequestMapping("/home")
-//    public Model showHomeTemplate(Model model){
-//        model.addAttribute("appName", appName);
-//        model.addAttribute("requests", jpaRequestDao.findAll());
-//        return model;
-//    }
-//}
+package com.galvanize.controllers;
+
+import com.galvanize.services.RequestService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class HomeController {
+
+    RequestService requestService;
+
+    @Value("${spring.application.name}")
+    String appName;
+
+    public HomeController (RequestService requestService){
+        this.requestService = requestService;
+    }
+
+    @RequestMapping("/")
+    public ModelAndView redirectHomeToHomePage(ModelMap model) {
+        return new ModelAndView("redirect:/home", model);
+    }
+
+
+    @RequestMapping("/home")
+    public Model showHomePage(Model model){
+        model.addAttribute("appName", appName);
+        model.addAttribute("officers", requestService.findAll());
+        return model;
+    }
+}
--- src/test/java/com/galvanize/controllers/HomeControllerTest.java
+++ src/test/java/com/galvanize/controllers/HomeControllerTest.java
@@ -1,25 +1,31 @@
-//package com.galvanize.controllers;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.web.servlet.MockMvc;
-//
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-//
-//@SpringBootTest
-//@AutoConfigureMockMvc
-//class HomeControllerTest {
-//
-//    @Autowired
-//    MockMvc mvc;
-//
-//    @Test
-//    public void testHomeController() throws Exception {
-//        String url = "/home";
-//        mvc.perform(get(url))
-//                .andExpect(status().isOk());
-//    }
-//}
+package com.galvanize.controllers;
+
+import com.galvanize.services.RequestService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class HomeControllerTest {
+
+    @Autowired
+    MockMvc mvc;
+
+
+    @Autowired
+    RequestService requestService;
+
+
+    @Test
+    public void testHomeController() throws Exception {
+        String url = "/home";
+        mvc.perform(get(url))
+                .andExpect(status().isOk());
+    }
+}
